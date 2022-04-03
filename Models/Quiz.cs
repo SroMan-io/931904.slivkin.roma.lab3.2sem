7b6ee15a62203dbe9b4402de9896dfe863389ba4
SroMan-io <85176272+SroMan-io@users.noreply.github.com>
2022-04-03 12:22:39 +0700

    Add files via upload

---
+++ Models/Quiz.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication1.Models
+{
+    public class Quiz
+    {      
+        public int x { get; set; }
+        public int y { get; set; }         
+        public char sign { get; set; }
+        public int res { get; set; }
+
+        public Random rand = new Random();       
+        public int signID;
+
+        public Quiz()
+        {
+            x = rand.Next(0, 10); y = rand.Next(0, 10); signID = rand.Next(0, 4);
+            if (signID == 0) { sign = '+';  res = (x + y); }
+            else if (signID == 1) { sign = '-'; res = (x - y); }
+            else if (signID == 2) { sign = '*'; res = (x * y); }
+            else {
+                if (y == 0) y++;
+                sign = '/'; res = (x / y); 
+            }                     
+        }    
+    }
+}
---
+++ Models/QuizCollection.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class QuizCollection
+    {
+       public List<Quiz> quiz = new List<Quiz>();
+       public List<int> answers = new List<int>();
+       public int questionsCount = 0;
+       public int rightAnswersCount = 0;
+       public void CountRightAnswers()
+       {
+            for (int i = 0; i < questionsCount; i++)
+                if (quiz[i].res == answers[i]) rightAnswersCount++;
+        }
+        public void Clear()
+        {
+            questionsCount = 0; 
+            rightAnswersCount = 0;
+            quiz.Clear();
+            answers.Clear();    
+        }
+    }
+}
